8499e199c3a244396709dd212072fbd74b449553
comountainclimber <swissninja56@apples-macbook-5.local>
2015-09-23 14:17:37 -0600

    second commit

--- main.js
+++ main.js
@@ -18,12 +18,12 @@ $scope.submit = function() {
 	console.log($scope.quotes)
 }
 
-$scope.quotes = [{quote: "For such small creatures such as we, the vastness is only bareable through love.",
+$scope.quotes = [{quote: "For such small creatures as we, the vastness is only bareable through love.",
 author: "Carl Sagan"}]
 
 
 $scope.quote = {
-	quote: "",
+	quote: "", 
 	author: ""
 }
 
@@ -34,6 +34,32 @@ $scope.openInput = function(){
 $scope.closeTheQuote = function(index) {
 	$scope.quotes.splice(index, 1)
 }
+
+$scope.closeInput = function(event){
+		$scope.hideInput=true;
+}
+
+$scope.escInput = function(event){
+		console.log(event.which)
+		if (event.which === 27) {
+			$scope.hideInput=true
+		}
+	
+}
+
+
+$scope.submitRating = function(index) {
+	console.log(index+1+ " many stars")
+}
+
+$scope.hideTheLightBox = function() {
+	$scope.hideInput=true
+}
+
+
+// $scope.submitQuote = function(){
+// 	$scope.hideInput=true
+// }
 
 // var stringify = function(quote){
 // 	for (var i = 0; i < $scope.quote.length; i++) {
